26826c9f592dafe1dab27c6330876ffff3bfaa5e
Mizore <92151449+Mizdere@users.noreply.github.com>
2024-06-16 16:39:51 -0700

    Add files via upload
    This program will write the name, address and telephone
    number of the programmer.

---
+++ name.cpp
@@ -0,0 +1,19 @@
+// This program will write the name, address and telephone
+// number of the programmer.
+
+// Last Modified Date: 3/22/2024
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{	cout << "************\n";
+	cout << "    Programmer:\tJohn Smith\n";
+	cout << "\t\t123 Real Street\n";
+	cout << "\t\tIrvine, CA 54321\n";
+	cout << "    Telephone:\t789-789-7789\n";
+	cout << "************";
+
+	return 0;
+}
